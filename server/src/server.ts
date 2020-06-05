fad1c3a5576a914ce6e8544b4f3f1dc22f72bc2a
Lucas Marques <lucas.msilva09@gmail.com>
2020-06-04 21:38:39 -0300

    Creating the routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-
-app.get('/users', (req, res) =>{
-    res.json({message: "Hello World!"})
-});
+app.use(routes);
 
 app.listen(3333);

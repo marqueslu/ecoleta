da9005f215feb97e6bae39cb473ba028fe215a01
Lucas Marques <lucas.msilva09@gmail.com>
2020-06-05 21:43:11 -0300

    Creating controllers and new methods

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = Router();
 
-routes.get('/users', (request, response) => {
-  response.json({ message: 'Hello World!' });
-});
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;

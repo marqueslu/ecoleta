6b5d7fe63507d6f15d2f7bf4bbbc90670a6c843d
Lucas Marques <lucas.msilva09@gmail.com>
2020-06-08 20:57:28 -0300

    Adding validations to the create points route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
+import { errors } from 'celebrate';
+
 const app = express();
 
 app.use(cors());
@@ -10,4 +12,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
 app.listen(3333);
